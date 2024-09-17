fcd9dbada3de924ebb8fc2c0e77e9a4bdb3280a0
Gabriel Amaral <gabriel2012amaral@gmail.com>
2024-09-16 23:01:50 -0300

    validação de token e criação de uma função que atualiza os dados do usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -6,6 +6,7 @@ const jwt = require("jsonwebtoken");
 // helpers
 const createUserToken = require("../helpers/create-user-token");
 const getToken = require("../helpers/get-token");
+const getUserByToken = require("../helpers/get-user-by-token");
 
 module.exports = class UserController {
   static async register(req, res) {
@@ -117,5 +118,95 @@ module.exports = class UserController {
     }
 
     res.status(200).send(currentUser);
+  }
+
+  static async getUserById(req, res) {
+    const id = req.params.id;
+
+    const user = await User.findById(id).select("-password");
+
+    if (!user) {
+      res.status(422).json({
+        message: "Usuário não encontrado",
+      });
+    }
+
+    res.status(200).json({ user });
+  }
+
+  static async editUser(req, res) {
+    const id = req.params.id;
+
+    // check if user exists
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    const { name, email, phone, password, confirmpassword } = req.body;
+
+    let image = "";
+
+    // validations
+    if (!name) {
+      res.status(422).json({ message: "O nome é obrigatório" });
+      return;
+    }
+
+    user.name = name;
+
+    if (!email) {
+      res.status(422).json({ message: "O email é obrigatório" });
+      return;
+    }
+
+    // check if email has already taken
+    const userExists = await User.findOne({ email: email });
+
+    if (user.email !== email && userExists) {
+      res.status(422).json({
+        message: "Por favor, utilize outro e-mail.",
+      });
+      return;
+    }
+
+    user.email = email;
+
+    if (!phone) {
+      res.status(422).json({ message: "O fone é obrigatório" });
+      return;
+    }
+
+    user.phone = phone;
+
+    if (!password) {
+      res.status(422).json({ message: "A senha é obrigatório" });
+      return;
+    }
+
+    if (password !== confirmpassword) {
+      res.status(422).json({ message: "As senhas não conferem!" });
+      return;
+    } else if (password === confirmpassword && password != null) {
+      // creating a password
+      const salt = await bcrypt.genSalt(12);
+      const passwordHash = await bcrypt.hash(password, salt);
+
+      user.password = passwordHash;
+    }
+
+    try {
+      // return user updated data
+      await User.findOneAndUpdate(
+        { _id: user._id },
+        { $set: user },
+        { new: true }
+      );
+
+      res.status(200).json({
+        message: "Usuário atualizado com sucesso",
+      });
+    } catch (error) {
+      res.status(500).json({ message: error });
+      return;
+    }
   }
 };
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,8 +2,12 @@ const router = require("express").Router();
 
 const UserController = require("../controllers/UserController");
 
+// middleware
+const verifyToken = require("../helpers/verify-token");
+
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.get("/checkuser", UserController.checkUser);
+router.patch("/edit/:id", verifyToken, UserController.editUser);
 
 module.exports = router;
