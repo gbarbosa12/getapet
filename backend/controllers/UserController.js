4a35b968c80bdc8b9d42996674ea6cb247156aa5
Gabriel Amaral <gabriel2012amaral@gmail.com>
2024-09-19 21:28:29 -0300

    Iniciando rotas de Pet

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -145,6 +145,10 @@ module.exports = class UserController {
 
     let image = "";
 
+    if (req.file) {
+      user.image = req.file.filename;
+    }
+
     // validations
     if (!name) {
       res.status(422).json({ message: "O nome é obrigatório" });
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,10 +4,16 @@ const UserController = require("../controllers/UserController");
 
 // middleware
 const verifyToken = require("../helpers/verify-token");
+const { imageUpload } = require("../helpers/image-upload");
 
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.get("/checkuser", UserController.checkUser);
-router.patch("/edit/:id", verifyToken, UserController.editUser);
+router.patch(
+  "/edit/:id",
+  verifyToken,
+  imageUpload.single("image"),
+  UserController.editUser
+);
 
 module.exports = router;
