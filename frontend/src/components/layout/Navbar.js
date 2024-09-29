6b03f4f770a95aa1b18a719c77e3b1c0e61ffaaa
Gabriel Amaral <gabriel2012amaral@gmail.com>
2024-09-29 10:35:33 -0300

    Criando função de login, logout e autenticação do usuário

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
 
 import Logo from "../../assets/img/logo.png";
 
 import styles from "./Navbar.module.css";
 
+// context
+import { Context } from "../../context/UserContext";
+
 function Navbar() {
+  const { authenticated, logout } = useContext(Context);
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbar_logo}>
@@ -15,12 +21,20 @@ function Navbar() {
         <li>
           <Link to="/">Adotar</Link>
         </li>
-        <li>
-          <Link to="/login">Entrar</Link>
-        </li>
-        <li>
-          <Link to="/register">Cadastrar</Link>
-        </li>
+        {authenticated ? (
+          <>
+            <li onClick={logout}>Sair</li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">Entrar</Link>
+            </li>
+            <li>
+              <Link to="/register">Cadastrar</Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
